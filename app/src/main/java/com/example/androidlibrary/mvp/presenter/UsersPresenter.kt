e7fb51ec121480ae4e7c6e52a773a54146d1dcbf
julia <j.kholstinina@yandex.ru>
2021-10-20 19:48:46 +0300

    implement RX in UserPresenter ans User List

--- app/src/main/java/com/example/androidlibrary/mvp/presenter/UsersPresenter.kt
+++ app/src/main/java/com/example/androidlibrary/mvp/presenter/UsersPresenter.kt
@@ -1,15 +1,19 @@
 package com.example.androidlibrary.mvp.presenter
 
+import android.util.Log
 import com.example.androidlibrary.mvp.model.GithubUser
 import com.example.androidlibrary.mvp.model.GithubUsersRepo
 import com.example.androidlibrary.mvp.view.IScreens
 import com.example.androidlibrary.mvp.view.IUserItemView
 import com.example.androidlibrary.mvp.view.UsersView
 import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import moxy.MvpPresenter
 
 class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val screen: IScreens) :
-    MvpPresenter<UsersView>() {
+        MvpPresenter<UsersView>() {
 
     class UsersListPresenter : IUserListPresenter {
         val users = mutableListOf<GithubUser>()
@@ -25,11 +29,13 @@ class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val scr
     }
 
     val usersListPresenter = UsersListPresenter()
+    val users = usersRepo.getUsers()
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
-        loadData()
+        exec()
+        viewState.updateList()
         usersListPresenter.itemClickListener = { userItemView ->
             openDetailedUserInfo(userItemView)
         }
@@ -39,14 +45,54 @@ class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val scr
         router.navigateTo(screen.detailedUser(userItemView.positionItem))
     }
 
-    private fun loadData() {
-        val users = usersRepo.getUsers()
-        usersListPresenter.users.addAll(users)
-        viewState.updateList()
-    }
-
     fun backPressed(): Boolean {
         router.exit()
         return true
+    }
+
+    private fun exec() {
+        Consumer(Producer()).execFromIterable()
+    }
+
+    //Observable
+    inner class Producer() {
+        fun fromIterable(): Observable<GithubUser> {
+            return Observable.fromIterable(users)
+        }
+    }
+
+    inner class Consumer(val producer: Producer) {
+
+        val userObserver = object : Observer<GithubUser> {
+            var disposable: Disposable? = null
+            override fun onSubscribe(d: Disposable?) {
+                disposable = d
+                Log.i(RX_TAG, d.toString())
+            }
+
+            override fun onNext(t: GithubUser?) {
+                Log.i(RX_TAG, t.toString())
+                t?.let {
+                    usersListPresenter.users.add(it)
+                }
+            }
+
+            override fun onError(e: Throwable?) {
+                Log.i(RX_TAG, e?.localizedMessage.toString())
+            }
+
+            override fun onComplete() {
+                Log.i(RX_TAG, "onCompete")
+            }
+        }
+
+        fun execFromIterable() {
+            producer.fromIterable()
+                    .subscribe(userObserver)
+        }
+    }
+
+    companion object {
+        const val RX_TAG = "RX_TAG"
     }
 }
